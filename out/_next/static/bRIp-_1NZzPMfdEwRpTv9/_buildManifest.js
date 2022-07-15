d1430c88f5df777b07f1bf10744781873e38f905
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2022-07-15 13:19:40 +0000

    commit

---
+++ out/_next/static/bRIp-_1NZzPMfdEwRpTv9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/15-bccff0d512d4cab1.js","static/chunks/pages/index-77464d469a55ce91.js"],"/_error":["static/chunks/pages/_error-fa7c6084170a5211.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
